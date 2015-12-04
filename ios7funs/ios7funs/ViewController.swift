91c1f84e3ee601867c2e31e8ee27c7ff1ca891fb
ayo1103 <ayo1103@gmail.com>
2015-12-04 15:30:04 +0800

    Create new project

---
+++ ios7funs/ios7funs/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ios7funs
+//
+//  Created by Bryan Lin on 12/4/15.
+//  Copyright © 2015 Giant Croissant. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
