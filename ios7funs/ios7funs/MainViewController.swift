567ed0daa8f0c5f1bceab10a81955555aef600a4
ayo1103 <ayo1103@gmail.com>
2015-12-08 12:00:39 +0800

    Add ShowInfos page Add Videos page Add Recipes page Add Collections page Add Q & A page Add Links page Add Bonus page
    Add Main page images
    
    Update Main Storyboard

--- ios7funs/ios7funs/MainViewController.swift
+++ ios7funs/ios7funs/MainViewController.swift
@@ -10,9 +10,30 @@ import UIKit
 
 class MainViewController: UIViewController {
 
+    @IBOutlet weak var btnShowInfos: UIButton!
+    @IBOutlet weak var btnVideos: UIButton!
+    @IBOutlet weak var btnRecipes: UIButton!
+    @IBOutlet weak var btnCollections: UIButton!
+    @IBOutlet weak var btnQandA: UIButton!
+    @IBOutlet weak var btnLinks: UIButton!
+    @IBOutlet weak var btnBonus: UIButton!
+
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        setImageToScaleAspectFill(btnShowInfos)
+        setImageToScaleAspectFill(btnVideos)
+        setImageToScaleAspectFill(btnRecipes)
+        setImageToScaleAspectFill(btnCollections)
+        setImageToScaleAspectFill(btnQandA)
+        setImageToScaleAspectFill(btnLinks)
+        setImageToScaleAspectFill(btnBonus)
     }
+
+    func setImageToScaleAspectFill(button: UIButton) {
+        button.imageView?.contentMode = UIViewContentMode.ScaleAspectFill
+    }
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
---
+++ ios7funs/ios7funs/Recipes/RecipesViewController.swift
@@ -0,0 +1,49 @@
+//
+//  RecipesViewController.swift
+//  ios7funs
+//
+//  Created by Bryan Lin on 12/8/15.
+//  Copyright © 2015 Giant Croissant. All rights reserved.
+//
+
+import UIKit
+
+class RecipesViewController: UIViewController {
+
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+
+        // Show navigation bar.
+        self.navigationController?.navigationBarHidden = false
+    }
+
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillAppear(animated)
+
+        // Hide navigation bar.
+        self.navigationController?.navigationBarHidden = true
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
