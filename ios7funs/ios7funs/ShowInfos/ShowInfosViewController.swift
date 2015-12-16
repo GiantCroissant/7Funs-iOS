71de9522b77b91f66f056585b9cd5d86c92cd39c
ayo1103 <ayo1103@gmail.com>
2015-12-16 14:37:16 +0800

    Add show intro storyboard

--- ios7funs/ios7funs/ShowInfos/ShowInfosViewController.swift
+++ ios7funs/ios7funs/ShowInfos/ShowInfosViewController.swift
@@ -9,6 +9,35 @@
 import UIKit
 
 class ShowInfosViewController: UIViewController {
+    @IBOutlet weak var page1: UIView!
+    @IBOutlet weak var page2: UIView!
+
+    @IBAction func onSegmentValueChange(sender: AYOSegmentedControl) {
+        switch (sender.selectedIndex) {
+        case 0:
+            showIntroPage()
+            break
+
+        case 1:
+            showTeachersPage()
+            break
+
+        default:
+            break
+        }
+    }
+
+    func showIntroPage() {
+        self.title = "節目介紹"
+        page1.hidden = false
+        page2.hidden = true
+    }
+
+    func showTeachersPage() {
+        self.title = "料理老師"
+        page1.hidden = true
+        page2.hidden = false
+    }
 
     override func viewDidAppear(animated: Bool) {
         super.viewDidAppear(animated)
@@ -20,7 +49,7 @@ class ShowInfosViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        self.title = "節目介紹"
+        showIntroPage()
     }
 
     override func didReceiveMemoryWarning() {
---
+++ ios7funs/ios7funs/ShowInfos/ShowIntroViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ShowIntroViewController.swift
+//  ios7funs
+//
+//  Created by Bryan Lin on 12/16/15.
+//  Copyright © 2015 Giant Croissant. All rights reserved.
+//
+
+import UIKit
+
+class ShowIntroViewController: UIViewController {
+
+
+    @IBOutlet weak var bgTime: UIView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+
+        bgTime.layer.cornerRadius = 5
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
