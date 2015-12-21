28bf99521138ea78c92f491324d49d57758c18dd
ayo1103 <ayo1103@gmail.com>
2015-12-21 17:34:42 +0800

    Update info.plist
    Test upload to Itunes Connect

---
+++ ios7funs/ios7funs/Login/RegisterViewController.swift
@@ -0,0 +1,17 @@
+//
+//  RegisterViewController.swift
+//  ios7funs
+//
+//  Created by Bryan Lin on 12/21/15.
+//  Copyright © 2015 Giant Croissant. All rights reserved.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    @IBAction func onBackClick(sender: UIButton) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+
+}
