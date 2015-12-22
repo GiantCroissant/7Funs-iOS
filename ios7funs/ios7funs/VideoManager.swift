c4260d72b78869697c5d52de12bb646b7242c33f
ayo1103 <ayo1103@gmail.com>
2015-12-22 13:51:31 +0800

    Add Video Manager

---
+++ ios7funs/ios7funs/VideoManager.swift
@@ -0,0 +1,17 @@
+//
+//  VideoManager.swift
+//  ios7funs
+//
+//  Created by Bryan Lin on 12/22/15.
+//  Copyright © 2015 Giant Croissant. All rights reserved.
+//
+
+import Foundation
+
+class VideoManager {
+
+    static let sharedInstance = VideoManager()
+
+    
+
+}
