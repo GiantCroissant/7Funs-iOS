aa5508a5f1e14c6c2dfd2e2f74c033cfcb3cd195
ayo1103 <ayo1103@gmail.com>
2016-01-07 02:40:40 +0800

    Add basic logic flow

--- ios7funs/ios7funs/Login/RegisterViewController.swift
+++ ios7funs/ios7funs/Login/RegisterViewController.swift
@@ -10,16 +10,36 @@ import UIKit
 
 class RegisterViewController: UIViewController {
 
+    @IBOutlet weak var inputEmail: UITextField!
+    @IBOutlet weak var inputPassword: UITextField!
+    @IBOutlet weak var inputUserName: UITextField!
+    @IBOutlet weak var btnSend: UIButton!
+
+    @IBAction func onValueChanged(sender: UISwitch) {
+        btnSend.enabled = sender.on
+    }
+
+    @IBAction func onSendRegistration(sender: UIButton) {
+        dLog("send registration to server")
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureSendButton()
+    }
+
+    func configureSendButton() {
+        btnSend.enabled = false
+    }
+
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
-
         self.title = "註冊"
     }
 
     override func viewDidAppear(animated: Bool) {
         super.viewDidAppear(animated)
-
         self.showNavigationBar()
     }
-
+    
 }
