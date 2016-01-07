f4118e8663a400548610a7519eabffaad7a7c401
ayo1103 <ayo1103@gmail.com>
2016-01-07 12:08:06 +0800

    Refacotr recipe VC

--- ios7funs/ios7funs/Recipes/RecipeTableViewCell.swift
+++ ios7funs/ios7funs/Recipes/RecipeTableViewCell.swift
@@ -10,35 +10,66 @@ import UIKit
 
 class RecipeTableViewCell: UITableViewCell {
 
-    // FIXME: should load from db
-    var added = false
+    //    // FIXME: should load from db
+    //    var added = false
 
     @IBOutlet weak var labelTitle: UILabel!
     @IBOutlet weak var imgFood: UIImageView!
     @IBOutlet weak var btnAddCollection: UIButton!
     @IBOutlet weak var btnFood: UIButton!
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-
-    // FIXME: should write data or delete this function here
-    @IBAction func onAddButtonClick(sender: UIButton) {
-        added = !added
-        if added {
-            let image = UIImage(named: "icon_love_m_pink")
-            sender.setImage(image, forState: .Normal)
-
-        } else {
-            let image = UIImage(named: "icon_love_m")
-            sender.setImage(image, forState: .Normal)
+    var recipe: RecipeUIModel! {
+        didSet {
+            configureRecipeCell()
         }
     }
+
+    private func configureRecipeCell() {
+        let imageId = recipe.imageId
+        let imageName = recipe.imageName
+        let title = recipe.title
+
+        // TODO: remove this line and set default image to background image view
+        //        ImageLoader.sharedInstance.loadDefaultImage("food_default") { image in
+        //            cell.imgFood.image = image
+        //        }
+
+        // FIXME: should change to row ?
+        imgFood.image = nil
+        imgFood.tag = imageId
+        labelTitle.text = title
+
+        //        btnAddCollection.tag = indexPath.row
+
+        RecipeManager.sharedInstance.loadFoodImage(imageId, imageName: imageName) { image, imageId, fadeIn in
+
+            if (imageId != self.imgFood.tag) {
+                return
+            }
+
+            self.imgFood.image = image
+            self.imgFood.alpha = 1
+
+            if fadeIn {
+                self.imgFood.alpha = 0
+                UIView.animateWithDuration(0.3) {
+                    self.imgFood.alpha = 1
+                }
+            }
+        }
+
+    }
+
+    //    // FIXME: should write data or delete this function here
+    //    @IBAction func onAddButtonClick(sender: UIButton) {
+    //        added = !added
+    //        if added {
+    //            let image = UIImage(named: "icon_love_m_pink")
+    //            sender.setImage(image, forState: .Normal)
+    //
+    //        } else {
+    //            let image = UIImage(named: "icon_love_m")
+    //            sender.setImage(image, forState: .Normal)
+    //        }
+    //    }
 }
--- ios7funs/ios7funs/Recipes/RecipesViewController.swift
+++ ios7funs/ios7funs/Recipes/RecipesViewController.swift
@@ -18,40 +18,35 @@ class RecipesViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         loadRecipes(onEmpty: {
-            dLog("empty local recipes")
             self.fetchMoreRecipes()
         })
-
-//      self.view.makeToast("加入收藏", duration: 10, position: .Top)
+        //      self.view.makeToast("加入收藏", duration: 10, position: .Top)
     }
 
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
-
         self.title = "食譜列表"
     }
 
     override func viewDidAppear(animated: Bool) {
         super.viewDidAppear(animated)
-
         self.showNavigationBar()
     }
 
     override func viewWillDisappear(animated: Bool) {
         super.viewWillAppear(animated)
-
         self.hideToastIndicator()
     }
 
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        navigationItem.title = ""
+        navigationItem.title = "" // TODO: this line should be move to destination VC ?
 
         let detailVC = segue.destinationViewController as! RecipeDetailViewController
         let row = (sender?.tag)!
         detailVC.recipe = recipes[row]
     }
+
 }
 
 // MARK: - Recpie Datas
@@ -61,8 +56,6 @@ extension RecipesViewController {
         if isFetching {
             return
         }
-
-        dLog("start fetching")
 
         isFetching = true
         UIUtils.showStatusBarNetworking()
@@ -77,8 +70,6 @@ extension RecipesViewController {
             onFinished: {
                 self.isFetching = false
                 UIUtils.hideStatusBarNetworking()
-
-                dLog("end fetching")
             }
         )
     }
@@ -101,95 +92,26 @@ extension RecipesViewController {
 // MARK: - UITableViewDataSource
 extension RecipesViewController: UITableViewDataSource {
 
-//    // FIXME: not working
-//    @IBAction func onAddButtonClick(sender: UIButton) {
-//        let row = sender.tag
-//        let recipe = recipes[row]
-//        recipe.added = !recipe.added
-//        if recipe.added {
-//            let image = UIImage(named: "icon_love_m_pink")
-//            sender.setImage(image, forState: .Normal)
-//
-//        } else {
-//            let image = UIImage(named: "icon_love_m")
-//            sender.setImage(image, forState: .Normal)
-//        }
-//    }
-
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return recipes.count
     }
 
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("idRecipeCell", forIndexPath: indexPath) as! RecipeTableViewCell
-
-        // FIXME: This line should be delete.
-        if (indexPath.row >= recipes.count) {
-            return cell
-        }
-
-        let recipe = recipes[indexPath.row]
-        let imageId = recipe.imageId
-        let imageName = recipe.imageName
-        let title = recipe.title
-
-        ImageLoader.sharedInstance.loadDefaultImage("food_default") { image in
-            cell.imgFood.image = image
-        }
-
-        // FIXME: should change to row ?
-        cell.imgFood.tag = imageId
         cell.btnFood.tag = indexPath.row
-
-        cell.labelTitle.text = title
-        cell.btnAddCollection.tag = indexPath.row
-
-        // TODO: finish this logic
-        /*
-        if recipe.added {
-            let image = UIImage(named: "icon_love_m_pink")
-            cell.btnAddCollection.setImage(image, forState: .Normal)
-
-        } else {
-            let image = UIImage(named: "icon_love_m")
-            cell.btnAddCollection.setImage(image, forState: .Normal)
-        }
-        */
-
-        RecipeManager.sharedInstance.loadFoodImage(imageId, imageName: imageName) { image, imageId, fadeIn in
-            if (cell.imgFood.tag == imageId) {
-                cell.imgFood.image = image
-                cell.imgFood.alpha = 1
-
-                if fadeIn {
-                    cell.imgFood.alpha = 0
-
-                    UIView.animateWithDuration(0.3) {
-                        cell.imgFood.alpha = 1
-                    }
-
-                } else {
-                    cell.imgFood.alpha = 1
-                }
-            }
-        }
+        cell.recipe = recipes[indexPath.row]
         return cell
     }
 }
 
 // MARK: - UITableViewDelegate
 extension RecipesViewController: UITableViewDelegate {
-
+    
     func scrollViewDidEndDecelerating(scrollView: UIScrollView) {
         let distFromBottom = scrollView.contentSize.height - scrollView.contentOffset.y
         if (distFromBottom <= scrollView.frame.height) {
             fetchMoreRecipes()
         }
     }
-
+    
 }
-
-
-
-
-
