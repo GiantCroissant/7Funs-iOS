7139d6636c84758479051e2b3493931e4dcd8989
ayo1103 <ayo1103@gmail.com>
2016-01-20 16:24:52 +0800

    Change Ucom link

--- ios7funs/ios7funs/Links/LinksViewController.swift
+++ ios7funs/ios7funs/Links/LinksViewController.swift
@@ -13,23 +13,25 @@ class LinksViewController: UIViewController {
     @IBOutlet weak var btnFriend01: UIButton!
     @IBOutlet weak var btnFriend02: UIButton!
 
+    let vcTitle = "吃飯好朋友"
+    let titleMitdub = "豆油伯"
+    let urlMitdub = "http://www.mitdub.com/"
+    let titleUcom = "Ucom"
+    let urlUcom = "https://www.facebook.com/rakenhouse/?fref=ts"
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         UIButton.scaleButtonImage(btnFriend01, mode: .Center, radius: 3)
         UIButton.scaleButtonImage(btnFriend02, mode: .Center, radius: 3)
     }
 
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
-
-        self.title = "吃飯好朋友"
+        self.title = vcTitle
     }
 
     override func viewDidAppear(animated: Bool) {
         super.viewDidAppear(animated)
-        
         self.showNavigationBar()
     }
 
@@ -38,14 +40,13 @@ class LinksViewController: UIViewController {
 
         if segue.identifier == "id_segue_link_friend_01" {
             let dstVC = segue.destinationViewController as! LinkFriendViewController
-            dstVC.url = "http://www.mitdub.com/"
-            dstVC.title = "豆油伯"
+            dstVC.url = urlMitdub
+            dstVC.title = titleMitdub
 
         } else if segue.identifier == "id_segue_link_friend_02" {
             let dstVC = segue.destinationViewController as! LinkFriendViewController
-//            dstVC.url = "http://mall.pchome.com.tw/store/QFAE7U"
-            dstVC.url = "http://www.rakenhouse.com/"
-            dstVC.title = "Ucom"
+            dstVC.url = urlUcom
+            dstVC.title = titleUcom
         }
     }
 
