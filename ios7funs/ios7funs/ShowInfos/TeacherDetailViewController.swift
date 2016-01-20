c34a3cd2d8836429b5ee207b7157a67c54b4956a
ayo1103 <ayo1103@gmail.com>
2016-01-20 18:33:13 +0800

    Add teacher layout UI

---
+++ ios7funs/ios7funs/ShowInfos/TeacherDetailViewController.swift
@@ -0,0 +1,35 @@
+//
+//  TeacherDetailViewController.swift
+//  ios7funs
+//
+//  Created by Bryan Lin on 1/20/16.
+//  Copyright © 2016 Giant Croissant. All rights reserved.
+//
+
+import UIKit
+
+class TeacherDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ ios7funs/ios7funs/ShowInfos/TeacherInfoDataView.swift
@@ -0,0 +1,14 @@
+//
+//  TeacherInfoDataView.swift
+//  ios7funs
+//
+//  Created by Bryan Lin on 1/20/16.
+//  Copyright © 2016 Giant Croissant. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class TeacherInfoDataView: NibDesignable {
+
+}
