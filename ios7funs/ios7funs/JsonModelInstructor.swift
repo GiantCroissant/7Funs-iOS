f48ebbb7b29a2ad5ec49a225fc58dc252af2b440
Apprentice <apprentice@Apprentices-MacBook-Pro.local>
2016-01-21 11:22:57 +0800

    Add instructor info json retrieving

---
+++ ios7funs/ios7funs/JsonModelInstructor.swift
@@ -0,0 +1,48 @@
+//
+//  JsonModelInstructor.swift
+//  ios7funs
+//
+//  Created by Apprentice on 1/21/16.
+//  Copyright © 2016 Giant Croissant. All rights reserved.
+//
+
+import Foundation
+
+import Argo
+import Curry
+
+public struct InstructorDetailJsonObject {
+    public let name: String
+    public let image: String
+    public let profileImage: String
+    public let experience: String
+    public let currentTitle: String
+    public let description: String
+}
+
+public struct InstructorJsonObject {
+    public let instructors: [InstructorDetailJsonObject]
+}
+
+extension InstructorDetailJsonObject : Decodable {
+    public static func decode(j: JSON) -> Decoded<InstructorDetailJsonObject> {
+        let f = curry(InstructorDetailJsonObject.init)
+            <^> j <| "name"
+            <*> j <| "name"
+            <*> j <| "profileImage"
+        
+        return f
+            <*> j <| "experience"
+            <*> j <| "currentTitle"
+            <*> j <| "description"
+    }
+}
+
+extension InstructorJsonObject : Decodable {
+    public static func decode(j: JSON) -> Decoded<InstructorJsonObject> {
+        let f = curry(InstructorJsonObject.init)
+            <^> j <|| "instructors"
+        
+        return f
+    }
+}
--- ios7funs/ios7funs/ShowInfos/ShowTeacherViewController.swift
+++ ios7funs/ios7funs/ShowInfos/ShowTeacherViewController.swift
@@ -8,19 +8,81 @@
 
 import UIKit
 
+import Argo
+import Curry
+import RxSwift
+
 class ShowTeacherViewController: UIViewController {
 
     @IBOutlet var photoBG: [UIView]!
+    
+    let infoDataSource: Observable<String> = Observable.create { (observer) in
+        let fileName = "Info"
+        let fileType = "json"
+        
+        let defaultContent = "{\"instructors\": []}"
+        
+        let path = NSBundle.mainBundle().pathForResource(fileName, ofType: fileType)
+        let content: String = path.map { x in
+            do {
+                let data = try String(contentsOfFile: x)
+                return data
+            } catch {
+                return defaultContent
+            }
+            } ?? defaultContent
+        
+        observer.on(.Next(content))
+        observer.on(.Completed)
+        return AnonymousDisposable {}
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        infoDataSource
+            .observeOn(BackgroundScheduler.instance())
+            .map({ (s: String) -> InstructorJsonObject? in
+                let data = s.dataUsingEncoding(NSUTF8StringEncoding)
+                
+                if let d = data {
+                    do {
+                        let jsonObject = try NSJSONSerialization.JSONObjectWithData(d, options: .AllowFragments) as! [String : AnyObject]
+                        let decoded = InstructorJsonObject.self.decode(JSON.parse(jsonObject))
+                        switch decoded {
+                        case .Success(let result):
+                            return result
+                            
+                        case .Failure:
+                            return nil
+                        }
+                    } catch {
+                        return nil
+                    }
+                }
+                
+                return nil
+            })
+            .observeOn(MainScheduler.instance)
+            .subscribeNext({ x in
+                // MARK: Should have all the data from info json
+                x?.instructors.forEach({ ijo in
+                    // Large image
+                    // ijo.image
+                    // Smaller image
+                    // ijo.profileImage
+                })
+                print(x)
+            })
+
 
         for photo in photoBG {
             photo.layer.cornerRadius = 5
         }
     }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
