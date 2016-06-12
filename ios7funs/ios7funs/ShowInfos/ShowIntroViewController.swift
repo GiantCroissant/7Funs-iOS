08d19e752a4b86bf7a08f298552f38e6d269e83f
ayo1103 <ayo1103@gmail.com>
2016-06-12 12:30:55 +0800

    update show info ui

--- ios7funs/ios7funs/ShowInfos/ShowIntroViewController.swift
+++ ios7funs/ios7funs/ShowInfos/ShowIntroViewController.swift
@@ -10,33 +10,56 @@ import UIKit
 
 class ShowIntroViewController: UIViewController {
 
-    @IBOutlet weak var bgTime: UIView!
-    @IBOutlet weak var lblDescription: UILabel!
+  @IBOutlet weak var showInfoLabel: UILabel!
+  @IBOutlet weak var platformInfoLabel: UILabel!
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        configureTimeBG()
-        configureDescription()
+  let lineHeight: CGFloat = 8.0
+
+  override func viewDidLayoutSubviews() {
+    super.viewDidLayoutSubviews()
+    configureLineSpacing(showInfoLabel)
+    configureLineSpacing(platformInfoLabel)
+  }
+
+  func getApproximateAdjustedFontSizeWithLabel(label: UILabel) -> CGFloat {
+    label.adjustsFontSizeToFitWidth = true
+    if label.adjustsFontSizeToFitWidth == true {
+      var currentFont: UIFont = label.font
+      let originalFontSize = currentFont.pointSize
+      var currentSize: CGSize = (label.text! as NSString).sizeWithAttributes([NSFontAttributeName: currentFont])
+      while currentSize.width > label.frame.size.width && currentFont.pointSize > (originalFontSize * label.minimumScaleFactor) {
+        currentFont = currentFont.fontWithSize(currentFont.pointSize - 1)
+        currentSize = (label.text! as NSString).sizeWithAttributes([NSFontAttributeName: currentFont])
+      }
+
+      return currentFont.pointSize
+
+    } else {
+      return label.font.pointSize
     }
+  }
 
-    func configureTimeBG() {
-        bgTime.layer.cornerRadius = bgTime.frame.height / 2
-    }
+  func configureLineSpacing(label: UILabel) {
+    let adjustedFontSize = getApproximateAdjustedFontSizeWithLabel(label)
+    let text = label.text!
+    let attrText = NSMutableAttributedString(string: text)
+    let paraStyle = NSMutableParagraphStyle()
+    paraStyle.lineSpacing = 8
+    paraStyle.alignment = .Center
 
-    func configureDescription() {
-        let text = lblDescription.text!
-        let attrText = NSMutableAttributedString(string: text)
-        let paraStyle = NSMutableParagraphStyle()
-        paraStyle.lineSpacing = 8
-        paraStyle.alignment = .Center
+    let font = UIFont.boldSystemFontOfSize(adjustedFontSize)
+    attrText.addAttribute(
+      NSFontAttributeName,
+      value: font,
+      range: NSRange(location: 0, length: attrText.length)
+    )
+    attrText.addAttribute(
+      NSParagraphStyleAttributeName,
+      value: paraStyle,
+      range: NSRange(location: 0, length: attrText.length)
+    )
+    label.attributedText = attrText
+  }
 
-        attrText.addAttribute(
-            NSParagraphStyleAttributeName,
-            value: paraStyle,
-            range: NSRange(location: 0, length: attrText.length)
-        )
-        lblDescription.attributedText = attrText
-    }
 
-    
 }
--- ios7funs/ios7funs/Utils/Utils.swift
+++ ios7funs/ios7funs/Utils/Utils.swift
@@ -423,3 +423,17 @@ extension UIView {
 
 }
 
+extension UILabel {
+
+  func setLineHeight(lineHeight: CGFloat) {
+    let text = self.text
+    if let text = text {
+      let attributeString = NSMutableAttributedString(string: text)
+      let style = NSMutableParagraphStyle()
+
+      style.lineSpacing = lineHeight
+      attributeString.addAttribute(NSParagraphStyleAttributeName, value: style, range: NSMakeRange(0, text.characters.count))
+      self.attributedText = attributeString
+    }
+  }
+}
